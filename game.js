118ccb3048029c65d35f7815355944bb047b5fdf
Almag3000 <alim11235813@gmail.com>
2025-06-01 18:43:55 +0300

    Initial commit

---
+++ game.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#fff";
+ctx.font = "20px Arial";
+ctx.fillText("Flappy Bird заглушка", 50, 240);
